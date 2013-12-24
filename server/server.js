0d1a622bee5a10cc0c9532c6a6e7664bb8944f35
Francesco Gallarotti <info@gallarotti.net>
2013-12-25 00:13:18 +0100

    project and lists working

--- server/server.js
+++ server/server.js
@@ -1,30 +1,27 @@
-var express = require("express"), 
-	cards = require("./routes/cards"),
-	workflows = require("./routes/workflows"),
-	projects = require("./routes/projects");
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path");
+
+// import various routes
+var projects = require("./routes/projects.js");
+var lists = require("./routes/lists.js");
+var cards = require("./routes/cards.js");
 
 var app = express();
 
-app.configure(function() {
-	app.use(express.logger("dev"));
-	app.use(express.bodyParser());
+// Express Config
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
-app.get("/cards", cards.findAll);
-app.get("/cards/state/:stateid/project/:projectid", cards.findByProjectIdStateId);
-app.get("/cards/:id", cards.findById);
-app.post("/cards", cards.addCard);
-app.put("/cards/:id", cards.updateCard);
-
-app.get("/workflows", workflows.findAll);
-app.get("/workflows/:id", workflows.findById);
-app.post("/workflows", workflows.addWorkflow);
-app.put("/workflows/:id", workflows.updateWorkflow);
 
 app.get("/projects", projects.findAll);
 app.get("/projects/:id", projects.findById);
-app.post("/projects", projects.addProject);
-app.put("/projects/:id", projects.updateProject);
+app.get("/lists/:id", lists.findAll);
+app.get("/cards/:id", cards.findAll);
 
 app.listen(3000);
 console.log("collaborative-minds server listening on port 3000...");
